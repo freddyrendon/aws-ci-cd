c7e7210d68395a486222bb39ba2a6fd49ad23a8f
Freddy Rendon <frendonjr03@gmail.com>
2023-10-21 19:26:14 -0400

    update

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello User! Python Flask App is Running from AWS EC2 ECR Docker. More tutorial: https://shawonruet.com"
+    return "Hello Team 8! Python Flask App is Running from AWS EC2. Lets GOOOO!"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5000)
